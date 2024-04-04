bd4808761e8c3c0272b11c7bbc21172a125a5e46
NwoluDavid <nwoludave@gmail.com>
2024-04-04 11:45:28 +0100

    added Annotated to routes

--- product Search/routes/index.py
+++ product Search/routes/index.py
@@ -1,13 +1,14 @@
 from fastapi import  APIRouter ,Query
 from controler.read_product import filter_products
+from typing import Annotated
 
 
 router = APIRouter()
 
 @router.get('/product')
-async def read_product(product_name:str =Query(default=None , description ="product name") ,
-category: str = Query(default=None , description= "product category" ), 
-price_range: str = Query(default=None , description="price range")):
+async def read_product( product_name: Annotated [str | None,  Query(description = 'Name of the product')] = None ,
+category: Annotated [ str | None , Query(description= "product category")]= None, 
+price_range: Annotated [ str | None , Query(description= "product category")]= None):
    
    """
    
